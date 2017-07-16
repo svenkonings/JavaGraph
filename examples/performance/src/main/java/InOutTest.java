f94412429d5848cc339c273c8589c05baf20c47c
meteoorkip <svenkonings@live.nl>
2017-07-16 20:52:54 +0200

    Comment the performance tests

--- examples/performance/src/main/java/InOutTest.java
+++ examples/performance/src/main/java/InOutTest.java
@@ -13,11 +13,16 @@ public class InOutTest {
         List<Long> outTimeSmall = new ArrayList<>();
         List<Long> inTimeLarge = new ArrayList<>();
         List<Long> outTimeLarge = new ArrayList<>();
+        // For the specified amount of times
         for (int i = 0; i < TIMES; i++) {
+            // Load the Java graph
             JavaMatcher matcher = new JavaMatcher("examples/performance/performance.gps");
+            // Get the first node
             JavaNode node = matcher.getGraph().visitNodes().iterator().next();
+            // Warming up
             matcher.getGraph().inEdgeSet(node);
             matcher.getGraph().outEdgeSet(node);
+            // Record the time it takes to retrieve the incoming and the outgoin edge sets
             {
                 long inStart = System.nanoTime();
                 matcher.getGraph().inEdgeSet(node);
@@ -28,11 +33,14 @@ public class InOutTest {
                 long outEnd = System.nanoTime();
                 outTimeSmall.add(outEnd - outStart);
             }
+            // Apply the rule (which adds a node to the end of a line of nodes) the specified amount of times
             for (int j = 0; j < AMOUNT; j++) {
                 matcher.applyMatch("rule");
             }
+            // Warming up
             matcher.getGraph().inEdgeSet(node);
             matcher.getGraph().outEdgeSet(node);
+            // Record the time it takes to retrieve the incoming and the outgoin edge sets
             {
                 long inStart = System.nanoTime();
                 matcher.getGraph().inEdgeSet(node);
@@ -43,12 +51,15 @@ public class InOutTest {
                 long outEnd = System.nanoTime();
                 outTimeLarge.add(outEnd - outStart);
             }
+            // Reset the nodes in the graph
             SimpleNode.nodes.clear();
         }
+        // Calculate the averages, ignoring the first half of the results
         double inSmallAvg = inTimeSmall.stream().mapToLong(Long::longValue).skip(AMOUNT / 2).average().orElse(0);
         double outSmallAvg = outTimeSmall.stream().mapToLong(Long::longValue).skip(AMOUNT / 2).average().orElse(0);
         double inLargeAvg = inTimeLarge.stream().mapToLong(Long::longValue).skip(AMOUNT / 2).average().orElse(0);
         double outLargeAvg = outTimeLarge.stream().mapToLong(Long::longValue).skip(AMOUNT / 2).average().orElse(0);
+        // Print the results
         System.out.println("InTimeSmall=" + inTimeSmall + ", avg=" + inSmallAvg);
         System.out.println("OutTimeSmall=" + outTimeSmall + ", avg=" + outSmallAvg);
         System.out.println("InTimeLarge=" + inTimeLarge + ", avg=" + inLargeAvg);
--- examples/performance/src/main/java/OutEdgeTest.java
+++ examples/performance/src/main/java/OutEdgeTest.java
@@ -11,23 +11,32 @@ public class OutEdgeTest {
     public static void main(String[] args) throws Exception {
         List<Long> javaTime = new ArrayList<>();
         List<Long> grooveTime = new ArrayList<>();
+        // For the specified amount of times
         for (int i = 0; i < TIMES; i++) {
+            // Load the Java graph
             JavaMatcher matcher = new JavaMatcher("examples/performance/performance.gps");
             long javaStart = System.nanoTime();
+            // Apply the rule (which adds a node to the end of a line of nodes) the specified amount of times
             for (int j = 0; j < AMOUNT; j++) {
                 matcher.applyMatch("rule");
             }
             long javaEnd = System.nanoTime();
+            // Record the time it took
             javaTime.add(javaEnd - javaStart);
             SimpleNode.nodes.clear();
+            // Load the GROOVE generator
             Generator generator = new Generator("-r=" + AMOUNT, "-s=linear", "-a=any", "examples/performance/performance.gps");
             long grooveStart = System.nanoTime();
+            // Apply the rule (which adds a node to the end of a line of nodes) the specified amount of times
             generator.start();
             long grooveEnd = System.nanoTime();
+            // Record the time it took
             grooveTime.add(grooveEnd - grooveStart);
         }
+        // Calculate the averages, ignoring the first half of the results
         double javaAvg = javaTime.stream().mapToLong(Long::longValue).skip(TIMES / 2).average().orElse(0);
         double grooveAvg = grooveTime.stream().mapToLong(Long::longValue).skip(TIMES / 2).average().orElse(0);
+        // Print the results
         System.out.println("JavaTime=" + javaTime + ", avg=" + javaAvg);
         System.out.println("GrooveTime=" + grooveTime + ", avg=" + grooveAvg);
     }
